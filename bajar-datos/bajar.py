035bc74e6e483ba1c34b2afd979f26b5258382e4
Nicolas Nadisic <nicolas.nadisic@gmail.com>
2016-08-14 00:30:18 -0500

    Adaptation for multipages request, reduce metadatas downloaded

--- bajar-datos/bajar.py
+++ bajar-datos/bajar.py
@@ -1,4 +1,5 @@
 from flickrapi import FlickrAPI
+import time
 import keys
 
 # Parameters definition:
@@ -10,22 +11,29 @@ ACCURACY = 11 # Accuracy to city level
 CONTENT_TYPE = 6 # Exclude screenshots from download
 PER_PAGE = 500 # Number of results per page (max 500)
 
-EXTRAS = 'description,license,date_upload,date_taken,owner_name,\
-icon_server,original_format,last_update,geo,tags,machine_tags,\
-o_dims,views,media,path_alias,url_s'
+# EXTRAS = 'description,license,date_upload,date_taken,owner_name,\
+# icon_server,original_format,last_update,geo,tags,machine_tags,\
+# o_dims,views,media,path_alias,url_s'
 
+EXTRAS = 'description,date_taken,owner_name,geo,tags,media,path_alias,url_s'
+
+NB_PAGES = 500
 
 ##############################################################################
 
 flickr = FlickrAPI(keys.API_KEY, keys.API_SECRET, format='parsed-json')
 
-for curpage in range(1):
-    bogota = flickr.photos.search(extras=EXTRAS,
+results = []
+
+for curpage in range(NB_PAGES):
+    print(curpage)
+    page = flickr.photos.search(extras=EXTRAS,
                                  place_id=BOGOTA_FLICKR_ID,
                                  accuracy=ACCURACY,
                                  content_type=CONTENT_TYPE,
                                  per_page=PER_PAGE,
                                  page=curpage)
-    photos = bogota['photos']
+    results.append(page['photos'])
+    time.sleep(1) # Tempo to avoid API restriction
 
-    print(photos)
+print(results)
