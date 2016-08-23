00dab1547c187b5e384b15de316cffb1d13abda4
Nicolas Nadisic <nicolas.nadisic@gmail.com>
2016-08-22 21:51:25 -0500

    Correcion del script, para producir un JSON correcto

--- bajar-datos/bajar.py
+++ bajar-datos/bajar.py
@@ -1,4 +1,5 @@
 from flickrapi import FlickrAPI
+import json
 import sys
 import time
 import keys
@@ -10,7 +11,7 @@ import keys
 BOGOTA_FLICKR_ID = '368148' # ID of Bogota in Flickr places database
 ACCURACY = 11 # Accuracy to city level
 CONTENT_TYPE = 6 # Exclude screenshots from download
-PER_PAGE = 500 # Number of results per page (max 500)
+PER_PAGE = 250 # Number of results per page (max 250)
 
 # EXTRAS = 'description,license,date_upload,date_taken,owner_name,\
 # icon_server,original_format,last_update,geo,tags,machine_tags,\
@@ -18,19 +19,20 @@ PER_PAGE = 500 # Number of results per page (max 500)
 
 EXTRAS = 'description,date_taken,owner_name,geo,tags,media,path_alias,url_s'
 
-NB_PAGES = 500
+NB_PAGES = 3
 
 ##############################################################################
 
 def eprint(*args, **kwargs):
+    '''Print in stderr'''
     print(*args, file=sys.stderr, **kwargs)
 
     
-flickr = FlickrAPI(keys.API_KEY, keys.API_SECRET, format='parsed-json')
+flickr = FlickrAPI(keys.API_KEY, keys.API_SECRET, format='json')
 
 results = []
 
-for curpage in range(NB_PAGES):
+for curpage in range(1, NB_PAGES+1):
     eprint(curpage)
     page = flickr.photos.search(extras=EXTRAS,
                                  place_id=BOGOTA_FLICKR_ID,
@@ -38,7 +40,15 @@ for curpage in range(NB_PAGES):
                                  content_type=CONTENT_TYPE,
                                  per_page=PER_PAGE,
                                  page=curpage)
-    results.append(page['photos'])
+    results.append(page.decode('utf-8'))
     time.sleep(1) # Tempo to avoid API restriction
 
-print(results)
+results_merged = []
+for res in results:
+    results_merged.append(json.loads(res))
+
+#print(results_merged)
+
+output = json.dumps(results_merged)
+
+print(output)
